b5d48c0835f4848b612ee9bc40ed6219987584dc
Joel Dickson <joel.dickson@agoda.com>
2020-10-23 10:59:43 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,45 +24,12 @@ if [ -z "$webhook_url" ]; then
     exit 1
 fi
 
-if [ -z "$webhook_secret" ]; then
-    echo "No webhook_secret configured"
+if [ -z "$data" ]; then
+    echo "No data configured"
     exit 1
 fi
 
-if [ -n "$webhook_type" ] && [ "$webhook_type" == "form-urlencoded" ]; then
-
-    EVENT=`urlencode "$GITHUB_EVENT_NAME"`
-    REPOSITORY=`urlencode "$GITHUB_REPOSITORY"`
-    COMMIT=`urlencode "$GITHUB_SHA"`
-    REF=`urlencode "$GITHUB_REF"`
-    HEAD=`urlencode "$GITHUB_HEAD_REF"`
-    WORKFLOW=`urlencode "$GITHUB_WORKFLOW"`
-
-    CONTENT_TYPE="application/x-www-form-urlencoded"
-    WEBHOOK_DATA="event=$EVENT&repository=$REPOSITORY&commit=$COMMIT&ref=$REF&head=$HEAD&workflow=$WORKFLOW"
-
-    if [ -n "$data" ]; then
-        WEBHOOK_DATA="${WEBHOOK_DATA}&${data}"
-    fi
-
-else
-
-    CONTENT_TYPE="application/json"
-
-    if [ -n "$webhook_type" ] && [ "$webhook_type" == "json-extended" ]; then
-        RAW_FILE_DATA=`cat $GITHUB_EVENT_PATH`
-        WEBHOOK_DATA=$(echo -n "$RAW_FILE_DATA" | jq -c '')
-    else
-        WEBHOOK_DATA="{\"event\":\"$GITHUB_EVENT_NAME\",\"repository\":\"$GITHUB_REPOSITORY\",\"commit\":\"$GITHUB_SHA\",\"ref\":\"$GITHUB_REF\",\"head\":\"$GITHUB_HEAD_REF\",\"workflow\":\"$GITHUB_WORKFLOW\"}"
-    fi
-    
-    if [ -n "$data" ]; then
-        CUSTOM_JSON_DATA=$(echo -n "$data" | jq -c '')
-        JSON_WITH_OPEN_CLOSE_BRACKETS_STRIPPED=`echo "$WEBHOOK_DATA" | sed 's/^{\(.*\)}$/\1/'`
-        WEBHOOK_DATA="{$JSON_WITH_OPEN_CLOSE_BRACKETS_STRIPPED,\"data\":$CUSTOM_JSON_DATA}"
-    fi
-
-fi
+WEBHOOK_DATA=$(echo -n "$data" | jq -c '')
 
 WEBHOOK_SIGNATURE=$(echo -n "$WEBHOOK_DATA" | openssl sha1 -hmac "$webhook_secret" -binary | xxd -p)
 WEBHOOK_ENDPOINT=$webhook_url
@@ -74,7 +41,6 @@ fi
 curl -k -v --fail \
     -H "Content-Type: $CONTENT_TYPE" \
     -H "User-Agent: User-Agent: GitHub-Hookshot/760256b" \
-    -H "X-Hub-Signature: sha1=$WEBHOOK_SIGNATURE" \
     -H "X-GitHub-Delivery: $GITHUB_RUN_NUMBER" \
     -H "X-GitHub-Event: $GITHUB_EVENT_NAME" \
     --data "$WEBHOOK_DATA" $WEBHOOK_ENDPOINT
